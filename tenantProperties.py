9f04ef1398f2061a7f2961f884bcddf7c9d363ba
aSandhu7893 <anu.sandhu7893@gmail.com>
2022-03-31 00:37:07 -0700

    updated tenantProperties endpoint

--- tenantProperties.py
+++ tenantProperties.py
@@ -6,14 +6,20 @@ from flask_jwt_extended import jwt_required, get_jwt_identity
 from data import connect
 import json
 
+
 class TenantProperties(Resource):
     decorators = [jwt_required()]
+
     def get(self):
         response = {}
         user = get_jwt_identity()
         with connect() as db:
-            where = {
-                'tenant_id': user['user_uid']
-            }
-            response = db.select('propertyInfo', where)
+            # where = {
+            #     'tenant_id': user['user_uid']
+            # }
+            response = db.execute("""SELECT * FROM pm.propertyInfo WHERE rental_status = 'ACTIVE' AND tenant_id = \'"""
+                                  + user['user_uid']
+                                  + """\'""")
+            # response = db.select(
+            #     "propertyInfo WHERE rental_status= 'ACTIVE'", where)
         return response
